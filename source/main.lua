5e50f1f39daea633913a632935175f498b25ae55
klumpmeister <christian@klumpmeister.net>
2023-09-06 12:52:42 -0400

    pong set up added ball and graphics

--- source/main.lua
+++ source/main.lua
@@ -1,8 +1,25 @@
+import "CoreLibs/object"
 import "CoreLibs/graphics"
+import "CoreLibs/sprites"
 
-local gfx <const> = playdate.graphics
 
-function playdate.update() 
-    gfx.clear()
-    gfx.drawText("Hello World", 20, 20)
+class("Ball").extends(playdate.graphics.sprite)
+
+
+function Ball:init()
+
+  Ball.super.init(self)
+  radius = 5
+  local ballImage = playdate.graphics.image.new(2 * radius, 2 * radius)
+  playdate.graphics.pushContext(ballImage)
+  playdate.graphics.fillCircleAtPoint(radius, radius, radius)
+  playdate.graphics.popContext()
+  self:setImage(ballImage)
+  self:moveTo(200, 120)
+end
+
+ball = Ball()
+ball:add()
+function playdate.update()
+    playdate.graphics.sprite.update()
 end
