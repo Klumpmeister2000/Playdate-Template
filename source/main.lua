d97af5c0afd30957b241ec0a01523ad2cc3d217a
klumpmeister <christian@klumpmeister.net>
2023-09-06 12:56:43 -0400

    added function with moveBy and abbrv. gfx

--- source/main.lua
+++ source/main.lua
@@ -2,24 +2,30 @@ import "CoreLibs/object"
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 
+gfx = playdate.graphics
 
-class("Ball").extends(playdate.graphics.sprite)
+class("Ball").extends(gfx.sprite)
 
 
 function Ball:init()
 
   Ball.super.init(self)
   radius = 5
-  local ballImage = playdate.graphics.image.new(2 * radius, 2 * radius)
-  playdate.graphics.pushContext(ballImage)
-  playdate.graphics.fillCircleAtPoint(radius, radius, radius)
-  playdate.graphics.popContext()
+  local ballImage = gfx.image.new(2 * radius, 2 * radius)
+  gfx.pushContext(ballImage)
+  gfx.fillCircleAtPoint(radius, radius, radius)
+  gfx.popContext()
   self:setImage(ballImage)
   self:moveTo(200, 120)
 end
 
+function Ball:update()
+    self:moveBy(1, 0)
+end
+
 ball = Ball()
 ball:add()
+
 function playdate.update()
-    playdate.graphics.sprite.update()
+    gfx.sprite.update()
 end
