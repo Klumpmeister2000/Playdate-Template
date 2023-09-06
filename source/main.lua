06204c31e5294115d2c559503b8f8a44f7ec65a9
klumpmeister <christian@klumpmeister.net>
2023-09-06 13:05:20 -0400

    corrected speed and playtested the bounce back

--- source/main.lua
+++ source/main.lua
@@ -10,8 +10,6 @@ class("Ball").extends(gfx.sprite)
 function Ball:init()
 
   Ball.super.init(self)
-
-  self.xSpeed = 5
   
   radius = 5
   local ballImage = gfx.image.new(2 * radius, 2 * radius)
@@ -20,14 +18,20 @@ function Ball:init()
   gfx.popContext()
   self:setImage(ballImage)
   self:moveTo(200, 120)
+  self.xSpeed = 5
 end
 
 function Ball:update()
 -- screen width = 400px
-    if self.x + self.xSpeed >= then
+    if self.x + self.xSpeed >= 400 
+        then
         self.xSpeed *= -1
-    elseif self.x + self.xSpeed <= 0 then
+    elseif self.x + self.xSpeed <= 0 
+        then
         self.xSpeed *= -1
+end
+
+self:moveBy(self.xSpeed, 0)
 end
 
 ball = Ball()
